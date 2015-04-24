148429901147ef23830c09a4fc58b3694fc090af
Enrico Steiger <enrico_steiger@gmx.de>
2015-04-24 09:52:15 +0200

    chinese routing instrcutions

--- js/list.js
+++ js/list.js
@@ -5,7 +5,7 @@
 list = {
 	languages: ['de','en','es','fr','it','nl', 'hu','ru' ,'cz','pl','ua','cn_simple','cn'], 
 	
-	routingLanguages : ['en', 'de', 'pl', 'bg', 'cz', 'nl', 'hr', 'hu', 'nl_BE', 'es', 'eo', 'fi', 'fr', 'it', 'pt_BR', 'ro', 'ru', 'se', 'dk', 'tr', 'ca', 'ja', 'no', 'vi', 'nb', 'de-rheinl', 'de-opplat', 'de-berlin', 'de-swabia', 'de-ruhrpo', 'de-at-ooe', 'de-bay'],
+	routingLanguages : ['en', 'de', 'pl', 'bg', 'cn','cn_simple','cz', 'nl', 'hr', 'hu', 'nl_BE', 'es', 'eo', 'fi', 'fr', 'it', 'pt_BR', 'ro', 'ru', 'se', 'dk', 'tr', 'ca', 'ja', 'no', 'vi', 'nb', 'de-rheinl', 'de-opplat', 'de-berlin', 'de-swabia', 'de-ruhrpo', 'de-at-ooe', 'de-bay'],
 		
 	distanceUnits: ['m', 'km', 'yd', 'mi'],
 	distanceUnitsPreferences : ['m', 'yd'],
