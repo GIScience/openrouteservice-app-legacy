09b6bc03b29c1326bcdae594f95cbd46e447b931
Enrico Steiger <enrico_steiger@gmx.de>
2015-06-02 16:07:19 +0200

    update release notes and namespaces

--- js/namespaces.js
+++ js/namespaces.js
@@ -37,14 +37,22 @@ namespaces.schemata = {
  */
 namespaces.services = {
 	// see latest ORS API documentation http://wiki.openstreetmap.org/wiki/OpenRouteService
-	
 	geocoding : 'http://openls.geog.uni-heidelberg.de/testing2015/geocoding', //for address search requests
 	routing : 'http://openls.geog.uni-heidelberg.de/testing2015/routing', //for routing requests
-	directory : 'http://openls.geog.uni-heidelberg.de/osm/directory-test', //for POI search requests
-	analyse : 'http://openls.geog.uni-heidelberg.de/testing2015/analysis',
-	shorten: 'http://openls.geog.uni-heidelberg.de/osm/shortenlink-test',
+	directory : 'http://openls.geog.uni-heidelberg.de/testing2015/directory', //for POI search requests
+	analyse : 'http://openls.geog.uni-heidelberg.de/testing2015/analysis', //for accessibility analysis requests
+	shorten: 'http://openls.geog.uni-heidelberg.de/testing2015/shortenlink', //for bitly shortlink conversion
 	wps : '' //for calculation of polygon around route for POI search
 	//profile: '' 			//for height profile of route
+	
+	/** ORS test instance
+	geocoding : 'http://openls.geog.uni-heidelberg.de/osm/geocoding-test', //for address search requests
+	routing: 'http://openls.geog.uni-heidelberg.de/osm/routing-test', //for routing requests
+	directory : 'http://openls.geog.uni-heidelberg.de/osm/directory-test', //for POI search requests
+	analyse : 'http://openls.geog.uni-heidelberg.de/osm/analysis-test', //for accessibility analysis requests
+	shorten: 'http://openls.geog.uni-heidelberg.de/osm/shortenlink-test', //for bitly shortlink conversion
+	*/
+	
 };
 
 /**
