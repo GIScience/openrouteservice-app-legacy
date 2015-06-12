054009f0579c61f88970e5a48017987629c473f7
stefanhahmann <stefan.hahmann@geog.uni-heidelberg.de>
2015-06-12 15:26:32 +0200

    bugfix wheelchair parameters xml transmitting

--- js/modules/Route.js
+++ js/modules/Route.js
@@ -53,7 +53,7 @@ var Route = ( function(w) {"use strict";
 			
 			writer.writeStartElement('xls:ExtendedRoutePreference');
 
-			writer.writeElementString('xls:WeightingMethod', extendedRoutePreferencesWeight || 'Fastest');
+			// writer.writeElementString('xls:WeightingMethod', extendedRoutePreferencesWeight || 'Fastest');
 
 			if (routePref == 'HeavyVehicle') {
 
@@ -82,14 +82,14 @@ var Route = ( function(w) {"use strict";
 							writer.writeElementString('xls:LoadCharacteristic', truckParams[4]);
 						writer.writeEndElement();
 					}
-
 			}
-				
+			
+			// Please note: do change order of params without also changing them in respective RouteService.xsd!
 			if (routePref === 'Wheelchair') {
 				//tracktype
-				if (wheelChairParams[2] != 'null') {
+				if (wheelChairParams[3] != 'null') {
 					writer.writeStartElement('xls:trackTypes');
-					writer.writeElementString('xls:trackType', wheelChairParams[2]);
+					writer.writeElementString('xls:trackType', wheelChairParams[3]);
 					writer.writeEndElement();
 				}
 				//surface
@@ -99,22 +99,22 @@ var Route = ( function(w) {"use strict";
 					writer.writeEndElement();
 				}
 				//smoothness
-				if (wheelChairParams[1] != 'null') {
+				if (wheelChairParams[4] != 'null') {
 					writer.writeStartElement('xls:smoothnessTypes');
-					writer.writeElementString('xls:smoothnessType', wheelChairParams[1]);
+					writer.writeElementString('xls:smoothnessType', wheelChairParams[4]);
 					writer.writeEndElement();
 				}
 				//incline
-				if (wheelChairParams[3] != 'null') {
-					writer.writeElementString('xls:incline', wheelChairParams[3]);
+				if (wheelChairParams[1] != 'null') {
+					writer.writeElementString('xls:incline', wheelChairParams[1]);
 				}
 				//sloped curb
-				if (wheelChairParams[4] != 'null') {
-					writer.writeElementString('xls:slopedCurb', wheelChairParams[4]);
+				if (wheelChairParams[2] != 'null') {
+					writer.writeElementString('xls:slopedCurb', wheelChairParams[2]);
 				}
 			}
-				//</xls:ExtendedRoutePreference>
 			
+			//</xls:ExtendedRoutePreference>			
 			writer.writeEndElement();
 
 
