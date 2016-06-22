7d7b914fbc991cec4c72b19f778a3f5221d7cc81
Rui Nunes <rui.nunes1985@gmail.com>
2016-06-23 00:54:10 +0200

    Fixed titles on preferences modal. Added bootstrap alerts to feedback modal and colors to input boxes, when mandatory fields are not filled.

--- js/contact_form.js
+++ js/contact_form.js
@@ -4,17 +4,51 @@ jQuery(document).ready(function($) {
         var email = $("#email").val();
         var message = $("#message").val();
         //checking for blank fields	
-        if (name === '' || email === '') {
-            alert("Please Fill Required Fields");
-        } else {
+        if (name === '' && email === '') {
+            $("#feedback_error_message").show();
+            $("#feedback_error_message_2").hide();
+            $("#feedback_error_message_3").hide();
+            $("#feedback_error_message_4").hide();
+            $("#feedback_success_message").hide();
+            $("#name").css("border","2px solid orange");
+            $("#email").css("border","2px solid orange");
+        } 
+        else if (name === ''){
+            $("#feedback_error_message_2").show();
+            $("#feedback_error_message").hide();
+            $("#feedback_error_message_3").hide();
+            $("#feedback_error_message_4").hide();
+            $("#feedback_success_message").hide();
+            $("#name").css("border","2px solid orange");
+            $("#email").css("border","2px solid green");
+        }
+        else if (email === ''){
+            $("#feedback_error_message_3").show();
+            $("#feedback_error_message_2").hide();
+            $("#feedback_error_message").hide();
+            $("#feedback_error_message_4").hide();
+            $("#feedback_success_message").hide();
+            $("#email").css("border","2px solid orange");
+            $("#name").css("border","1px solid green");
+        }
+        else {
             // Returns successful data submission message when the entered information is stored in database.
+            $("#feedback_success_message").show();
+            $("#feedback_error_message").hide();
+            $("#feedback_error_message_2").hide();
+            $("#feedback_error_message_3").hide();
+            $("#feedback_error_message_4").hide();
+            $("#email").val("");
+            $("#name").val("");
+            $("#message").val("");
             $.post("php/contact_form.php", {
                 name1: name,
                 email1: email,
                 message1: message
-            }, function(data) {
+            }/*, function(data) {
                 $('#feedbackModal').modal('hide');
-            });
+            }*/);
         }
     });
 });
+
